238310bc91744b5cc9402267c8bb8689261dec22
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-07-16 00:58:47 +0600

    server is created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Mobile Wallet!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
